078f008d000b053fc492e98ee2ce1c4d0d5600f8
Cr1m50nr34p3r <akshatgarg789@gmail.com>
2022-01-30 12:26:56 +0530

    Added copy options and removed bloat

--- Pirate/pirate.sh
+++ Pirate/pirate.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 query=$( rofi -dmenu -l 0 -p "Search Torrent: "| tr ' ' '+' )
 base_url="https://1337x.wtf"
-declare -a torrent_links
-declare -a torrent_names
-declare -a cache_files
 declare -A torrent_urls
 notify-send "getting list of available torrents"
 curl -s "$base_url/search/$query/1/" -o "$HOME/.cache/1337x.wtf"
-cache_files+=("1337x.wtf")
-torrent_links_tmp=$(grep -Eo '"\/torrent\/[0-9]{7}\/.*/"' "$HOME/.cache/1337x.wtf" | sed 's/"\(.*\)"/\1/g') 
-for link in $torrent_links_tmp
+for link in $(grep -Eo '"\/torrent\/[0-9]{7}\/.*/"' "$HOME/.cache/1337x.wtf" | sed 's/"\(.*\)"/\1/g')
 do
-    torrent_links+=("$link")
-    torrent_names+=("$(echo "$link" | cut -d '/' -f4)")
-     
+    torrent_urls["$( echo "$link" | cut -d '/' -f4 )"]="$link" 
 done
-for ((i=0 ; i < ${#torrent_names[@]} ; i++))
-do
-    torrent_urls["${torrent_names[$i]}"]="${torrent_links[$i]}"
-done
-selection=$(echo "${torrent_names[@]}" | tr ' ' '\n' | rofi -dmenu -i -l 10 -p "Select Torrent")
+selection=$(echo "${!torrent_urls[@]}" | tr ' ' '\n' | rofi -dmenu -i -p "Select Torrent: ")
 notify-send "getting magnet link ...."
+echo "${torrent_urls[@]}"
 curl -s "$base_url${torrent_urls[$selection]}" -o "$HOME/.cache/1337x.wtf"
 magnet_link=$(grep -Eo '"magnet:\?xt=urn:btih:[0-9a-fA-F]{40,}.*" ' "$HOME/.cache/1337x.wtf"  | sed 's/"\(.*\)"/\1/')
 notify-send "got magnet link"
-tool_opt=$(echo "stream download" | tr ' ' '\n' | rofi -dmenu -l 2 )
+tool_opt=$(echo "STREAM DOWNLOAD COPY" | tr ' ' '\n' | rofi -dmenu -i )
 case $tool_opt in
-    download ) qbittorrent "$magnet_link" ;;
-    stream ) webtorrent "$magnet_link" ;; 
+    DOWNLOAD ) qbittorrent "$magnet_link" && notify-send "Downloading torrent for $selection" ;;
+    STREAM ) webtorrent "$magnet_link" --mpv && notify-send "Streaming torrent for $selection" ;; 
+    COPY | * ) echo "$magnet_link" | xclip -select clipboard  && notify-send "Magnet link for $selection has been copied to clipboard"
 esac 
-
-
+rm -fv "$HOME/.cache/1337x.wtf"
+notify-send "Enjoy Watching !!!"
